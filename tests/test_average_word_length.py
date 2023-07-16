8f5bc556d642f1071e22f32b43e93fc640ebcccb
vijayendra bhati <vijayendra.bhati@gmail.com>
2023-07-16 20:48:52 +0100

    Average word length + test cases

--- tests/test_average_word_length.py
+++ tests/test_average_word_length.py
@@ -1,10 +1,9 @@
 import unittest
-
+import os
 from src import average_word_length
 import findspark
-findspark.init()
 
-import os
+findspark.init()
 
 # Get the absolute path to the project's root directory
 project_root = os.path.abspath(os.path.dirname(__file__))
@@ -12,7 +11,7 @@ project_root = os.path.abspath(os.path.dirname(__file__))
 
 class AverageWordLengthTestCase(unittest.TestCase):
     def test_average_word_length(self):
-        input_file = os.path.join(project_root,"inputs/avg_word_length.txt")
+        input_file = os.path.join(project_root, "inputs/avg_word_length.txt")
         expected_average = 5.24  # Define the expected average word length
 
         # Call the function and get the result
@@ -20,6 +19,7 @@ class AverageWordLengthTestCase(unittest.TestCase):
 
         # Assert that the result matches the expected average
         self.assertAlmostEqual(result, expected_average, delta=0.01)
+
 
 if __name__ == "__main__":
     unittest.main()
