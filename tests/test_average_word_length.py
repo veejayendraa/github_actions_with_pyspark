25c0bfccecd0511ae5eb9bceff2da974eddced25
vijayendra bhati <vijayendra.bhati@gmail.com>
2023-07-16 20:19:29 +0100

    Average word length + test cases

--- tests/test_average_word_length.py
+++ tests/test_average_word_length.py
@@ -7,7 +7,7 @@ findspark.init()
 class AverageWordLengthTestCase(unittest.TestCase):
     def test_average_word_length(self):
         input_file = "inputs/avg_word_length.txt"
-        expected_average = 5.25  # Define the expected average word length
+        expected_average = 5.24  # Define the expected average word length
 
         # Call the function and get the result
         result = calculate_average_word_length(input_file)
